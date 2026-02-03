06abb9197adf4b3cb0eab8cce152a1088cf7614a
Dustin <redhotblood1010@gmail.com>
2026-02-03 11:47:39 -0500

    added things and such to the screen and code

--- M3Login_Dustin/ViewController.swift
+++ M3Login_Dustin/ViewController.swift
@@ -8,7 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UNText: UITextField!
+    @IBOutlet weak var PWText: UITextField!
+    @IBAction func LoginButton(_ sender: Any)
+    {
+        let userName = UNText.text!
+        let password = PWText.text!
+        
+        if userName == "DustinLAY" && password == "21laydus"
+        {
+            LSText.text = "Login Successful"
+            ColorView.backgroundColor = .green
+            ColorView.alpha = 0.5
+        }
+        else
+        {
+            LSText.text = "Login Failed"
+            ColorView.backgroundColor = .red
+            ColorView.alpha = 0.5
+        }
+    }
+    @IBOutlet weak var LSText: UILabel!
+    @IBOutlet weak var ColorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
